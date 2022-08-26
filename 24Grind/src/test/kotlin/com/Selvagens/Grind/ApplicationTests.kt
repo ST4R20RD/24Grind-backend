561c8b1ef66634e02edc2c882331712a918a4833
Gallen98 <allen98.gp@gmail.com>
2022-08-26 19:08:12 +0100

    spring initializr first commit

---
+++ 24Grind/src/test/kotlin/com/Selvagens/Grind/ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.Selvagens.Grind
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
